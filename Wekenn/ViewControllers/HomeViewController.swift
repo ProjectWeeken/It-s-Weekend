86a8b1169de91f560033f26a0e08ed6e96d60ac0
IsaSam <isaacsamuel27@gmail.com>
2018-12-13 03:11:50 -0500

    add all image to view

--- Wekenn/ViewControllers/HomeViewController.swift
+++ Wekenn/ViewControllers/HomeViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Parse
 
+let Ums = Umodel.all()
+
 class HomeViewController: UIViewController {
     @IBOutlet var listView: UIScrollView!
     @IBOutlet var bgImage: UIImageView!
@@ -19,6 +21,44 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if listView.subviews.count < Ums.count {
+            listView.viewWithTag(0)?.tag = 1000 //prevent confusion when looking up images
+            setupList()
+        }
+        
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    //add all images to the list
+    func setupList() {
+        
+        for i in Ums.indices {
+            
+            //create image view
+            let imageView  = UIImageView(image: UIImage(named: Ums[i].image))
+            imageView.tag = i
+            imageView.contentMode = .scaleAspectFill
+            imageView.isUserInteractionEnabled = true
+            imageView.layer.cornerRadius = 20.0
+            imageView.layer.masksToBounds = true
+            listView.addSubview(imageView)
+            
+            //attach tap detector
+            imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapImageView)))
+        }
+        
+        listView.backgroundColor = UIColor.clear
+        positionListItems()
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
