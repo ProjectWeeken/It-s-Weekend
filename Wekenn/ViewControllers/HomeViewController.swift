552798d05a403ee6772a03710f3c65cf5da3d2b9
Lkerry <annekerrieleveille@yahoo.fr>
2018-12-14 17:01:01 -0500

    Add Tab Bar
    Add different option Tab bar
    -Discover
    -Profile
    -Dates
    -Matches
    -Likers

--- Wekenn/ViewControllers/HomeViewController.swift
+++ Wekenn/ViewControllers/HomeViewController.swift
@@ -16,12 +16,18 @@ class HomeViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func loadHomeScreen(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let loggedInViewController = storyBoard.instantiateViewController(withIdentifier: "homeViewID") as! HomeViewController
+        self.present(loggedInViewController, animated: true, completion: nil)
 
-    override func didReceiveMemoryWarning() {
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
 
+}
 }
